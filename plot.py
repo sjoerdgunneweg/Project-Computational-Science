3eacc3479a93ebd8a7eb6dce6fa8c8776911f614
rinjile <rinji-le@hotmail.com>
2023-02-01 19:17:03 +0100

    Added code for code review + small changes

--- plot.py
+++ plot.py
@@ -1,13 +1,15 @@
 """
 Authors:     Sjoerd Gunneweg; Rinji Le; Pjotr Piet
 ID:          13133330; 13344552; 12714933
-Date:        31-01-2023
+Date:        January 31, 2023
 Description:
 This file plots the results of the experiments. The plots are saved in the
 'plots' folder.
 
 Usage:
 python3 plot.py
+or
+python3 plot.py -code_review
 
 NOTE: It takes a longer time than as expected, because the plots are saved
 with a high resolution.
@@ -15,6 +17,7 @@ with a high resolution.
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 
 def get_times(xs, filename):
@@ -82,11 +85,12 @@ def plot_num_fish(filename='plots/num_fish_loner_time', show=False):
     data_file = 'results/num_fish_results_loner_time.csv'
     times, ci_low, ci_high = get_times(num_fish, data_file)
 
-    plt.title('The percentage of time spent as a loner', fontweight='bold')
+    plt.title('The average percentage of time spent as a loner',
+              fontweight='bold')
     plt.xlabel('number of fish')
     plt.ylabel('time (%)')
 
-    plt.plot(num_fish, times, 'o-')
+    plt.plot(num_fish, times, '-')
     plt.fill_between(num_fish, ci_low, ci_high, alpha=0.2, label='95% CI')
 
     plt.legend(loc='best')
@@ -170,12 +174,12 @@ def plot_spawn_left_results(filename='plots/spawn_left_right_time',
     data_file = 'results/spawn_left_results_right_time.csv'
     times, ci_low, ci_high = get_times(tunnel_height, data_file)
 
-    plt.title('The percentage of time spent on the right side',
+    plt.title('The average percentage of time spent on the right side',
               fontweight='bold')
     plt.xlabel('tunnel height')
     plt.ylabel('time (%)')
 
-    plt.plot(tunnel_height, times, 'o-')
+    plt.plot(tunnel_height, times, '-')
     plt.fill_between(tunnel_height, ci_low, ci_high, alpha=0.2, label='95% CI')
 
     plt.legend(loc='best')
@@ -204,7 +208,7 @@ def plot_num_clusters(filename='plots/num_clusters', show=False):
             time, data_file, num_fish, skip_cols=3)
         plt.plot(time, num_clusters, 'o-', label=f'{num_fish} fish')
 
-    plt.title('The number of clusters over time',
+    plt.title('The average number of clusters over time',
               fontweight='bold')
     plt.xlabel('time')
     plt.ylabel('number of clusters')
@@ -236,7 +240,7 @@ def plot_tunnel_height_num_clusters(
             time, data_file, height, skip_cols=3)
         plt.plot(time, num_clusters, 'o-', label=f'tunnel height: {height}')
 
-    plt.title('The number of clusters over time',
+    plt.title('The average number of clusters over time',
               fontweight='bold')
     plt.xlabel('time')
     plt.ylabel('number of clusters')
@@ -316,11 +320,15 @@ def plot_spawn_left_num_clusters(filename='plots/spawn_left_num_clusters',
 
 
 if __name__ == '__main__':
-    plot_num_fish()
-    plot_tunnel_height_results()
-    plot_tunnel_width_results()
-    plot_spawn_left_results()
-    plot_num_clusters()
-    plot_tunnel_height_num_clusters()
-    plot_tunnel_width_num_clusters()
-    plot_spawn_left_num_clusters()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-code_review':
+            plot_tunnel_height_num_clusters('code_review')
+    else:
+        plot_num_fish()
+        plot_tunnel_height_results()
+        plot_tunnel_width_results()
+        plot_spawn_left_results()
+        plot_num_clusters()
+        plot_tunnel_height_num_clusters()
+        plot_tunnel_width_num_clusters()
+        plot_spawn_left_num_clusters()
